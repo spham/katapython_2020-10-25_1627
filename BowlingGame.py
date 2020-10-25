bb66376bbd5bb925a4ecf3fc81a72f067667a534
Sylvain PHAM <sylvain@pham.es>
2020-10-25 17:09:06 +0100

    wip

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,3 +1,12 @@
 class BowlingGame:
     def __init__(self):
         self.rolls = []
+        
+    def roll(self, pins):
+        self.rolls.append(pins)
+    
+    def score(self):
+        result = 0
+        for i in range(20):
+            result += self.rolls[i]
+        return result
--- testBowlingGame.py
+++ testBowlingGame.py
@@ -2,11 +2,19 @@ import unittest
 import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.game = BowlingGame.BowlingGame()
+    
     def testCreateGame(self):
         game = BowlingGame.BowlingGame()
         
     def testGutterGale(self):
-        game = BowlingGame.BowlingGame()
         for i in range(20):
-            game.roll(0)
-        assert game.score() == 0
+            self.game.roll(0)
+        assert self.game.score() == 0
+    def testAllOnes(self):
+        for i in range(20):
+            self.game.roll(1)
+        assert self.game.score() == 20
+        
